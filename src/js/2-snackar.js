ee2f0c367dc49f6abc590b9b26a9ddf3eb0e1d02
Makaka449 <145019010+Makaka449@users.noreply.github.com>
2024-04-16 22:22:47 +0300

    Update 2-snackar.js

---
+++ src/js/2-snackar.js
@@ -0,0 +1,36 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+function createNotification(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === "fulfilled") {
+        resolve(delay);
+      } else if (state === "rejected") {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
+
+document.getElementById("notification-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const delay = parseInt(document.getElementById("delay-input").value);
+  const state = document.querySelector('input[name="state"]:checked').value;
+
+  createNotification(delay, state)
+    .then((delay) => {
+      iziToast.success({
+        title: "Notification",
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: "topRight",
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: "Notification",
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: "topRight",
+      });
+    });
+});
